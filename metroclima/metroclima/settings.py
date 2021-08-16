9cc96664e8319849206c367513b7ab19519a66db
Marcia Marques <marcia.marques@alumni.usp.br>
2021-08-16 17:30:59 -0300

    Use python decouple to set settings.py parameters

--- metroclima/metroclima/settings.py
+++ metroclima/metroclima/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rp4wmun&ion-3+9zt7(i@%-gy2ty!9@3^5r&d6@67a_mxeiq82'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['10.130.0.51', 'www.metroclima.iag.usp.br', '127.0.0.1']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 
 # Application definition
