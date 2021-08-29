62cf65ce619d59b411b7f14b3ae384b24929d44f
Marcia Marques <marcia.marques@alumni.usp.br>
2021-08-28 21:36:08 -0300

    Add change password option

--- metroclima/accounts/urls.py
+++ metroclima/accounts/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-
+    path('change-password/',
+         auth_views.PasswordChangeView.as_view(template_name='accounts/password.html'),
+         name='change_password'),
+    path('success/',
+         auth_views.PasswordChangeDoneView.as_view(template_name='accounts/success.html'),
+         name='password_change_done'),
 ]
