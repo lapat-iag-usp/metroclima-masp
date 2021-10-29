9e30f0ac8916cfedd86b5bb13161a05db661fe84
Marcia Marques <marcia.marques@alumni.usp.br>
2021-10-29 13:29:29 -0300

    Remove serial number from instrument __str__

--- metroclima/stations/models.py
+++ metroclima/stations/models.py
@@ -10,7 +10,7 @@ class Instrument(models.Model):
     slug = models.SlugField(unique=True, blank=True, null=True)
 
     def __str__(self):
-        return self.instrument + " " + self.serial_number
+        return self.instrument
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.instrument)
