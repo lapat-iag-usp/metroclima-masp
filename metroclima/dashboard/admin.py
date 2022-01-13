138cdcca9e8aceb3f152710873b23f3615f1240e
“Noele <noele.leonardo@gmail.com>
2022-01-13 10:15:07 -0300

    Create logbook model

--- metroclima/dashboard/admin.py
+++ metroclima/dashboard/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
+from django.db import models
+from django.forms import Textarea
 
-from .models import Campaign,  CampaignFile
+from .models import Campaign,  CampaignFile,\
+                    Flag, \
+                    Event, EventFile,\
+                    Logbook
 
 
 class CampaignFileInline(admin.TabularInline):
@@ -17,4 +22,38 @@ class CampaignAdmin(admin.ModelAdmin):
         js = ('/static/admin/js/hide_attribute_campaign.js',)
 
 
+class EventFileInline(admin.TabularInline):
+    model = EventFile
+
+
+class EventAdmin(admin.ModelAdmin):
+    readonly_fields = ('name', 'slug',)
+    list_display = ('name', 'event_date', 'description', 'invalid', 'start_date', 'end_date', 'flags', 'revised')
+    fields = ['name', 'logbook', 'event_date', 'description', 'invalid', 'start_date', 'end_date', 'flags', 'revised']
+    list_filter = ('logbook', 'event_date', 'invalid', 'revised', 'flags')
+    inlines = [EventFileInline]
+
+    class Media:
+        js = ('/static/admin/js/hide_attribute.js',)
+
+
+class EventInline(admin.TabularInline):
+    model = Event
+    fields = ['event_date', 'description', 'invalid', 'start_date', 'end_date', 'flags', 'revised']
+    ordering = ['event_date']
+    formfield_overrides = {
+        models.TextField: {'widget': Textarea(attrs={'rows': 6, 'cols': 40})},
+    }
+    show_change_link = True
+
+
+class LogbookAdmin(admin.ModelAdmin):
+    readonly_fields = ('name', 'slug',)
+    inlines = [EventInline]
+
+
 admin.site.register(Campaign, CampaignAdmin)
+admin.site.register(Flag)
+admin.site.register(Event, EventAdmin)
+admin.site.register(EventFile)
+admin.site.register(Logbook, LogbookAdmin)
--- metroclima/dashboard/models.py
+++ metroclima/dashboard/models.py
@@ -44,3 +44,81 @@ class CampaignFile(models.Model):
         super(CampaignFile, self).delete(*args, **kwargs)
         storage.delete(path)
 
+
+class Flag(models.Model):
+    flag = models.CharField(max_length=3)
+    description = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.flag
+
+
+class Logbook(models.Model):
+    name = models.CharField(max_length=50)
+    station = models.ForeignKey(Station, on_delete=models.CASCADE, blank=True)
+    campaign = models.ForeignKey(Campaign, on_delete=models.CASCADE)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.name = "Logbook " + self.campaign.name
+        self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=50, blank=True)
+    logbook = models.ForeignKey(Logbook, on_delete=models.CASCADE)
+    event_date = models.DateField(help_text="Please use the following format: YYYY-MM-DD.")
+    description = models.TextField(max_length=1000)
+    invalid = models.BooleanField(default=False)
+    start_date = models.DateTimeField(null=True, blank=True)
+    end_date = models.DateTimeField(null=True, blank=True)
+    flags = models.ForeignKey(Flag, on_delete=models.CASCADE, null=True, blank=True)
+    revised = models.BooleanField(default=False)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        qs = Event.objects.filter(event_date=self.event_date)
+        if not qs:
+            n = 1
+        else:
+            my_list = ([int(str(event)[-2:]) for event in qs])
+            n = sorted(my_list)[-1] + 1
+        if not self.name:
+            self.name = self.logbook.name + " " + str(self.event_date) + " " + f"{n:02d}"
+        else:
+            self.name = self.name
+        self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
+
+
+def event_file_path(instance, filename):
+    qs = EventFile.objects.filter(event=instance.event)
+    if not qs:
+        n = 1
+    else:
+        my_list = ([int(str(event).split('.')[0][-2:]) for event in qs])
+        n = sorted(my_list)[-1] + 1
+    ext = filename.split('.')[-1]
+    filename = f"files/logbook/{str(instance.event.logbook).replace(' ', '_')}/{instance.event.name}_file{n:02d}.{ext}"
+    return filename
+
+
+class EventFile(models.Model):
+    file = models.FileField(upload_to=event_file_path)
+    description = models.CharField(max_length=500)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.file.url
+
+    def delete(self, *args, **kwargs):
+        storage, path = self.file.storage, self.file.path
+        super(EventFile, self).delete(*args, **kwargs)
+        storage.delete(path)
